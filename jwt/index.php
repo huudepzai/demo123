af0189eaa75c1b4979b7f33e1a53ea1487a09512
huudv <dinhhuugt@gmail.com>
2019-07-15 10:09:11 +0700

    update try cach

--- jwt/index.php
+++ jwt/index.php
@@ -5,20 +5,22 @@ use \Firebase\JWT\JWT;
 $key = $_GET['key'];
 $jwt = $_GET['access_token'];
 
+try {
+    $decoded = JWT::decode($jwt, $key, array('HS256'));
 
-$decoded = JWT::decode($jwt, $key, array('HS256'));
-
-foreach ($decoded->data as $key => $item){
-    if (is_string($item)){
-        echo "<b>$key:</b> $item<br/>";
-    } else {
-        echo "<hr width='250px' align='left' /><b>".$key.'</b><br/>';
-        foreach ($item as $k=>$v){
-            echo "<b>$k:</b> $v<br/>";
+    foreach ($decoded->data as $key => $item){
+        if (is_string($item)){
+            echo "<b>$key:</b> $item<br/>";
+        } else {
+            echo "<hr width='250px' align='left' /><b>".$key.'</b><br/>';
+            foreach ($item as $k=>$v){
+                echo "<b>$k:</b> $v<br/>";
+            }
         }
     }
+} catch(Exception $e){
+    echo 'Error: ',  $e->getMessage(), "\n";
 }
-
 
 
 ?>

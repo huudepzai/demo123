155b3339eb9bdc9794fd8c43aa2233296626a3aa
huudv <dinhhuugt@gmail.com>
2019-07-15 09:35:28 +0700

    huudepzai

--- jwt/index.php
+++ jwt/index.php
@@ -1,17 +1,24 @@
 <?php
+include('./src/JWT.php');
 use \Firebase\JWT\JWT;
 
 $key = $_GET['key'];
-/**
- * IMPORTANT:
- * You must specify supported algorithms for your application. See
- * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
- * for a list of spec-compliant algorithms.
- */
 $jwt = $_GET['access_token'];
+
+
 $decoded = JWT::decode($jwt, $key, array('HS256'));
 
-print_r($decoded);
+foreach ($decoded->data as $key => $item){
+    if (is_string($item)){
+        echo "<b>$key:</b> $item<br/>";
+    } else {
+        echo "<b>".$key.'</b><br/>';
+        foreach ($item as $k=>$v){
+            echo "<b>$k:</b> $v<br/>";
+        }
+    }
+}
+
 
 
 ?>

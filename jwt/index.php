86944c556a2acd6c4a928d4df362a6c4a3ef1dc0
huudepzai <dinhhuugt@gmail.com>
2019-07-15 09:19:24 +0700

    update demo
    update demo

--- jwt/index.php
+++ jwt/index.php
@@ -8,7 +8,7 @@ $key = $_GET['key'];
  * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
  * for a list of spec-compliant algorithms.
  */
-
+$jwt = $_GET['access_token'];
 $decoded = JWT::decode($jwt, $key, array('HS256'));
 
 print_r($decoded);

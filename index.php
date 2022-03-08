68e2a178b0c81e6560ff0169c42466cb8fb3648e
Hữu Đinh <dinhhuugt@gmail.com>
2022-03-08 14:53:59 +0700

    test sso

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php 
-if($_REQUEST['login_id']){
-  $loginid = $_REQUEST['login_id'];
-}else{
-  $loginid = "";
-}
-$array = [
-  'access_token' => '21312343rbvhbvgbfhgbfhgbfh'.$$loginid
-];
-  echo json_encode($array);
+// if($_REQUEST['login_id']){
+//   $loginid = $_REQUEST['login_id'];
+// }else{
+//   $loginid = "";
+// }
+// $array = [
+//   'access_token' => '21312343rbvhbvgbfhgbfhgbfh'.$$loginid
+// ];
+//   echo json_encode($array);
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "http://test-onlineacademy.lms.local/benefit-one/sso">
+        <input name="sid" placeholder="sid">
+        <input name="cid" placeholder="cid">
+        <input type="submit" value="SEND">
+
+    </form>
+</body>
+</html>
